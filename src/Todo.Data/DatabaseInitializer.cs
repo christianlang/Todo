a67af7c026ef3bb9678b290b759c6743620da2bc
Christian Lang <christian_lang@gmx.de>
2012-03-10 14:56:06 +0100

    initial todos

--- src/Todo.Data/DatabaseInitializer.cs
+++ src/Todo.Data/DatabaseInitializer.cs
@@ -16,86 +16,28 @@
         {
             base.Seed(context);
 
-            var cv = context.Tags.Add(new Tag { Name = "CV" });
-            var job = context.Tags.Add(new Tag { Name = "job application" });
             var shopping = context.Tags.Add(new Tag { Name = "shopping list" });
 
             int id = 0;
             context.TodoItems.Add(
                 new TodoItem
-                    {
-                        Id = ++id,
-                        Text = "write CV",
-                        DueDate = new DateTime(2012, 2, 24),
-                        Tags = new[] { cv, job }, 
-                        Done = true
-                    });
-            context.TodoItems.Add(
-                new TodoItem
-                    {
-                        Id = ++id,
-                        Text = "take picture (photographer)",
-                        DueDate = new DateTime(2012, 2, 23),
-                        Tags = new[] { cv, job },
-                        Done = true
-                    });
-            context.TodoItems.Add(
-                new TodoItem
-                    {
-                        Id = ++id,
-                        Text = "edit picture",
-                        DueDate = new DateTime(2012, 2, 23),
-                        Tags = new[] { cv, job },
-                        Done = true
-                    });
-            context.TodoItems.Add(
-                new TodoItem
-                {
-                    Id = ++id,
-                    Text = "scan diploma",
-                    DueDate = new DateTime(2012, 2, 25),
-                    Tags = new[] { job },
-                    Done = true
-                });
-            context.TodoItems.Add(
-                new TodoItem
-                {
-                    Id = ++id,
-                    Text = "download MCPD transcript",
-                    DueDate = new DateTime(2012, 2, 25),
-                    Tags = new[] { job },
-                    Done = true
-                });
-            context.TodoItems.Add(
-                new TodoItem
-                {
-                    Id = ++id,
-                    Text = "send application",
-                    DueDate = new DateTime(2012, 2, 26),
-                    Tags = new[] { job },
-                    Done = true
-                });
-            context.TodoItems.Add(
-                new TodoItem
-                {
-                    Id = ++id,
-                    Text = "wait to be interviewed",
-                    Tags = new[] { job }
-                });
-            context.TodoItems.Add(
-                new TodoItem
-                {
-                    Id = ++id,
-                    Text = "quit my current job",
-                    DueDate = new DateTime(2012, 3, 31),
-                    Tags = new[] { job }
-                });
-            context.TodoItems.Add(
-                new TodoItem
                 {
                     Id = ++id,
                     Text = "milk",
-                    DueDate = DateTime.Today.AddDays(1),
+                    Tags = new[] { shopping }
+                });
+            context.TodoItems.Add(
+                new TodoItem
+                {
+                    Id = ++id,
+                    Text = "butter",
+                    Tags = new[] { shopping }
+                });
+            context.TodoItems.Add(
+                new TodoItem
+                {
+                    Id = ++id,
+                    Text = "eggs",
                     Tags = new[] { shopping }
                 });
             context.TodoItems.Add(
@@ -103,7 +45,7 @@
                 {
                     Id = ++id,
                     Text = "call Daniel",
-                    DueDate = DateTime.Today.AddDays(6)
+                    DueDate = DateTime.Today.AddDays(3)
                 });
         }
     }
